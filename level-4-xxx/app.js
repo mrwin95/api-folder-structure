5ff50b7ed71234399b65a713c8f13b9fd31c1481
Thang Nguyen <mrwin95@gmail.com>
2024-07-17 18:19:31 -0600

    added structure for level 4

---
+++ level-4-xxx/app.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const app = express();
+app.use();
+app.use("/api/events", events.API);
---
+++ level-4-xxx/server.js
@@ -0,0 +1,6 @@
+const app = require("../app");
+const http = require("http");
+
+// create server
+
+const server = htpp.createServer(app);
